fd7e8eac94fb6b0228faa75226f06db700ebdd76
schnee <hhhhp52@gmail.com>
2022-12-07 23:31:34 +0800

    [Update] 1. Login Page 2. Http Request/Response

--- main.py
+++ main.py
@@ -1,62 +1,93 @@
 import tkinter as tk
 import tkinter.constants as cs
 
+from api import api
+from utils.helper import get_host_by_env
+
+
+def _create_failed_tk():
+    failed_login_app = tk.Tk()
+    failed_login_app.title("Login Failed")
+    failed_login_app.geometry("240x200")  # width, height
+    failed_login_app.resizable(False, False)
+    failed_label = tk.Label(failed_login_app, text="Login Failed")
+    failed_label.pack(fill=cs.BOTH)
+    return failed_login_app
+
 
 def _create_tk():
     app = tk.Tk()
-    app.title("Schnee")
-    app.geometry("480x400")  # width, height
+    app.title("Generator Beta")
+    app.geometry("720x480")  # width, height
     app.resizable(False, False)
     return app
 
 
-def _init_layout_tk():
-    func = GUIFunc()
-    reset_button = tk.Button(text="Reset", command=func.reset_frame)
-    # Layout: can use pack(relative)/grid(row)/place(absolute)
-
-    # side = top/bottom/right/left
-    # test_button_2 = tkinter.Button(text="Test2")
-    # test_button_2.place(x=200, y=200, anchor=cs.CENTER)
-
-    # Can't use grid, when using the pack
-    # test_button_3 = tkinter.Button(text="Test3")
-    # test_button_3.grid(row=0, column=0)
+def _init_homepage_layout(env, account, password):
+    print("_init_homepage_layout")
+    print(env, account, password)
+    # HomePageGUIFunc()
 
 
-class GUIFunc:
+def _init_login_layout():
+    LoginGUIFunc()
+
+
+class LoginGUIFunc:
 
     def __init__(self):
-        welcome_button = tk.Button(text="Welcome", command=self.print_welcome)
-        welcome_button.pack(side=cs.TOP)
-        self.frame = None
-        self.welcome_button = welcome_button
-        self.reset_button = None
+        env_variable = tk.StringVar()
+        env_variable.set("env")
+        home_page_frame = tk.Frame(relief=cs.RIDGE, borderwidth=2, padx=2, pady=2, width=400, height=400)
+        env_label = tk.Label(home_page_frame, text="Environment: ")
+        env_label.grid(row=0, column=0)
+        local_radio_button = tk.Radiobutton(home_page_frame, variable=env_variable, text="local", value="local")
+        local_radio_button.select()
+        local_radio_button.grid(row=0, column=1)
+        dev_radio_button = tk.Radiobutton(home_page_frame, variable=env_variable, text="development", value="dev")
+        dev_radio_button.grid(row=0, column=2)
+        staging_radio_button = tk.Radiobutton(home_page_frame, variable=env_variable, text="staging", value="stg")
+        staging_radio_button.grid(row=0, column=3)
+        account_label = tk.Label(home_page_frame, text="Account: ")
+        account_label.grid(row=1, column=0)
+        account_entry = tk.Entry(home_page_frame)
+        account_entry.grid(row=1, column=1)
+        password_label = tk.Label(home_page_frame, text="Password: ")
+        password_label.grid(row=2, column=0)
+        password_entry = tk.Entry(home_page_frame)
+        password_entry.grid(row=2, column=1)
+        login_button = tk.Button(home_page_frame, text="Login", command=self.login)
+        login_button.grid(row=3, column=1)
+        home_page_frame.pack(anchor=cs.CENTER)
+        self.home_page_frame = home_page_frame
+        self.account_entry = account_entry
+        self.password_entry = password_entry
+        self.env_variable = env_variable
 
-    def print_welcome(self):
-        if self.frame is None:
-            frame = tk.Frame(relief=cs.RIDGE, borderwidth=2, padx=2, pady=2, width=200, height=200)
-            frame.pack(anchor=cs.CENTER)
-            self.frame = frame
-            label = tk.Label(frame, text="Hello, World")
-            label.pack(side=cs.TOP)
-            reset_button = tk.Button(text="Reset", command=self.reset_frame)
-            reset_button.pack(side=cs.BOTTOM)
-            self.reset_button = reset_button
-            self.welcome_button.destroy()
-            self.welcome_button = None
+    def login(self):
+        account = self.account_entry.get()
+        password = self.password_entry.get()
+        env = self.env_variable.get()
+        host = get_host_by_env(env)
+        if host:
+            data = dict(
+                account=account,
+                password=password
+            )
 
-    def reset_frame(self):
-        self.frame.destroy()
-        self.frame = None
-        self.reset_button.destroy()
-        self.reset_button = None
-        welcome_button = tk.Button(text="Welcome", command=self.print_welcome)
-        welcome_button.pack(side=cs.TOP)
-        self.welcome_button = welcome_button
+            flag, message, data = api.login(host, data)
+            if flag:
+                _init_homepage_layout(env, account, password)
+            else:
+                failed_login_app = _create_failed_tk()
+                failed_login_app.mainloop()
+
+        else:
+            failed_login_app = _create_failed_tk()
+            failed_login_app.mainloop()
 
 
 if __name__ == '__main__':
-    window = _create_tk()
-    _init_layout_tk()
-    window.mainloop()
+    app = _create_tk()
+    _init_login_layout()
+    app.mainloop()
