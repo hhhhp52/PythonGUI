3f0e67260c1668cd09d2a3bc62d8a73cbf7e7515
hhhhp52 <hhhhp52@gmail.com>
2022-12-07 17:44:43 +0800

    Update shift frame

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-import tkinter
+import tkinter as tk
 import tkinter.constants as cs
 
 
 def _create_tk():
-    window = tkinter.Tk()
-    window.title("Schnee")
-    window.geometry("480x400")  # width, height
-    window.resizable(False, False)
-    return window
+    app = tk.Tk()
+    app.title("Schnee")
+    app.geometry("480x400")  # width, height
+    app.resizable(False, False)
+    return app
 
 
 def _init_layout_tk():
     func = GUIFunc()
-    welcome_button = tkinter.Button(text="Welcome", command=func.print_welcome)
-    reset_button = tkinter.Button(text="Reset", command=func.reset_frame)
+    reset_button = tk.Button(text="Reset", command=func.reset_frame)
     # Layout: can use pack(relative)/grid(row)/place(absolute)
 
     # side = top/bottom/right/left
-    welcome_button.pack(side=cs.TOP)
-    reset_button.pack(side=cs.BOTTOM)
     # test_button_2 = tkinter.Button(text="Test2")
     # test_button_2.place(x=200, y=200, anchor=cs.CENTER)
 
@@ -29,20 +26,34 @@ def _init_layout_tk():
 
 class GUIFunc:
 
-    def __init__(self, frame=None):
-        self.frame = frame
+    def __init__(self):
+        welcome_button = tk.Button(text="Welcome", command=self.print_welcome)
+        welcome_button.pack(side=cs.TOP)
+        self.frame = None
+        self.welcome_button = welcome_button
+        self.reset_button = None
 
     def print_welcome(self):
         if self.frame is None:
-            frame = tkinter.Frame(relief=cs.RIDGE, borderwidth=2, padx=2, pady=2, width=200, height=200)
+            frame = tk.Frame(relief=cs.RIDGE, borderwidth=2, padx=2, pady=2, width=200, height=200)
             frame.pack(anchor=cs.CENTER)
             self.frame = frame
-            label = tkinter.Label(frame, text="Hello, World")
+            label = tk.Label(frame, text="Hello, World")
             label.pack(side=cs.TOP)
+            reset_button = tk.Button(text="Reset", command=self.reset_frame)
+            reset_button.pack(side=cs.BOTTOM)
+            self.reset_button = reset_button
+            self.welcome_button.destroy()
+            self.welcome_button = None
 
     def reset_frame(self):
         self.frame.destroy()
         self.frame = None
+        self.reset_button.destroy()
+        self.reset_button = None
+        welcome_button = tk.Button(text="Welcome", command=self.print_welcome)
+        welcome_button.pack(side=cs.TOP)
+        self.welcome_button = welcome_button
 
 
 if __name__ == '__main__':
